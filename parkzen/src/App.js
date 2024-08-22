4638c43bf9d4e78df9eab23f0c20d9179584f6e6
sakthi-1918 <sakthisangameswaran@gmail.com>
2024-08-22 19:37:41 +0530

    navigationbar

--- parkzen/src/App.js
+++ parkzen/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import NavBar from './components/NavBar';
+import Navibar from './components/Navigationbar';
 import {Routes,Route} from "react-router-dom";
 import LoginSignup from './components/signin';
 import LocationDropdown from './components/Dropdown';
@@ -8,11 +8,11 @@ function App() {
   const locations = ['Pioneer Mill', 'A block', 'IM parking', 'E block', 'N Block Ground'];
   return (
     <Routes>
-      <Route path="/" element={<NavBar/>}/>
+      <Route path="/" element={<Navibar/>}/>
       <Route path="/signin" element={<LoginSignup/>}/>
       <Route path="/location" element={ <LocationDropdown locations={locations} />}/>
     </Routes>
-    // <NavBar/>
+  
 
   );
 }
---
+++ parkzen/src/components/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems=[
+    {
+        title:"Home",
+        url:"/",
+        cName:"nav-links",
+        icon:"fa-solid fa-house-user"
+    },
+    {
+        title:"Parking Areas",
+        url:"/location",
+        cName:"nav-links",
+        icon:"fa-solid fa-car"
+    },
+    {
+        title:"About",
+        url:"/about",
+        cName:"nav-links",
+        icon:"fa-solid fa-circle-info"
+    },
+    {
+        title:"Contact",
+        url:"/contact",
+        cName:"nav-links",
+        icon:"fa-solid fa-address-book"
+    },
+    {
+        title:"Logout",
+        url:"/logout",
+        cName:"nav-links-mobile",
+        
+    },
+]
