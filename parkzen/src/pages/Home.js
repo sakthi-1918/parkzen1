af56500f31164dfbc8af6bf1f40affb2f0153cec
rubasundar <rubasundar2002@gmail.com>
2024-09-03 08:47:27 +0530

    homepage

--- parkzen/src/pages/Home.js
+++ parkzen/src/pages/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import  Navibar from '../components/Navigationbar';
-
+import Navibar from '../components/Navigationbar';
+import backgroundImage from '../assets/parkingLot.jpeg'; // Import the image
 
 function Home() {
+  const divStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover', // Cover the entire div
+    backgroundPosition: 'center', // Center the image
+    height: '100vh', // Full viewport height
+    width: '100vw', // Full viewport width
+    overflow: 'hidden', // Optional: to prevent scrolling bars
+    
+  };
+
   return (
-    <div>
-      
+    <div style={divStyle}>
       <Navibar />
-      
-      
+      {/* Add any additional content here */}
     </div>
   );
 }
